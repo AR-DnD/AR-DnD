38e40b03e96b724ea256e29c2aac6ca5eb5a8f59
CAM <cbraunstein@brandeis.edu>
2017-09-29 19:44:08 -0400

    Added Basic Map Database

---
+++ app/views/maps/_map.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! map, :id, :name, :data, :created_at, :updated_at
+json.url map_url(map, format: :json)
---
+++ test/system/maps_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MapsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit maps_url
+  #
+  #   assert_selector "h1", text: "Map"
+  # end
+end
