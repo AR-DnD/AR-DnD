4cc410fb2aeb3a446497cbd6900d59424d91254c
CAM <cbraunstein@brandeis.edu>
2017-10-06 15:49:52 -0400

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20171002040041) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "maps", force: :cascade do |t|
@@ -31,7 +30,7 @@ ActiveRecord::Schema.define(version: 20171002040041) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -42,7 +41,7 @@ ActiveRecord::Schema.define(version: 20171002040041) do
     t.string "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "forum_id"
   end
 
   create_table "users", force: :cascade do |t|
