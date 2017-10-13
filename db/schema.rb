fb50a17e5e8a6458fca89ee4fdadefdca102e2e0
CAM <cbraunstein@brandeis.edu>
2017-10-13 16:55:28 -0400

    Added story feild

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002040041) do
+ActiveRecord::Schema.define(version: 20171013203453) do
 
   create_table "forums", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171002040041) do
     t.string "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "story"
   end
 
   create_table "posts", force: :cascade do |t|
