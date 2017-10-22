bb5a4b0816b6f5d017476ada32560ee7e21f727d
Akiba Sato <akibasato1992@gmail.com>
2017-10-22 18:27:10 -0400

    Added sign up modal.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,26 @@ class SessionsController < ApplicationController
 
     end
 
+    # def create
+    #     user = User.find_by(email: params[:session][:email].downcase)
+    #     if user && user.authenticate(params[:session][:password])
+    #         session[:user_id] = user.id
+    #         flash[:success] = "You have successfully logged in."
+    #         redirect_to root_path
+    #     else
+    #         flash.now[:danger] = "There was something wrong with your login information."
+    #         render 'new'
+    #     end
+    # end
+
+
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-            session[:user_id] = user.id
-            flash[:success] = "You have successfully logged in."
+            log_in user
             redirect_to root_path
         else
-            flash.now[:danger] = "There was something wrong with your login information."
+            flash.now[:danger] = 'Invalid email/password combination'
             render 'new'
         end
     end
