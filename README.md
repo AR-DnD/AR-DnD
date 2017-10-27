f0299e357598b84c6fe46cb367a266fae09af130
BruceWilliamChen <brucewilliam1994@gmail.com>
2017-10-27 11:26:26 -0400

    some simple changes to tests and readme

--- README.md
+++ README.md
@@ -20,9 +20,7 @@ This repo is dedicated to the AR-DnD project and used Ruby on Rails and Unity 3D
 
 The web application build by Ruby on Rails is to serve as a front-end interface. Players can use the map generator to configure on the map layout, which contains the info where to place different game objects including goblins, trees and walls.
 
-Heroku: http://ar-dnd.herokuapp.com/
-
-Sample - [Under development...]
+Sample Exhibition- [Under development...]
 
 ### mobile-end iOS application :iphone:
 
@@ -32,6 +30,6 @@ An iOS application serving as the platform to display the AR-featured map and al
 
 - [X] Create a functional web front-end interface for players to put in login info and configure on maps
 - [X] Working on Unity 3D models using ARKit to create AR models for maps
-- [ ] Start to build the iOS application that will be able to display the maps
+- [X] Start to build the iOS application that will be able to display the maps
 - [X] Network connection between web and mobile end; mobile end pulls data from the database
 - [ ] Deploy!
---
+++ test/models/user_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+	def setup
+		@user = User.new(name: "whatever", email: "whatever")
+	end
+
+	test "name is too long" do
+		@user.name = "a"*16
+		assert_not @user.valid?
+	end
+
+	test "name is too short" do
+		@user.name = "a"*2
+		assert_not @user.valid?
+	end
+
+	test "email is too long" do
+		@user.email = "a"*104 + "@whatever.com"
+		assert_not @user.valid?
+	end
+
+	test "email should accept a valid email format" do
+		valid_addresses = %w[whatever@whatever.com WHATEVER@whatever.COM W_HAT-EVER@whatever.org 
+			                 what.ever@whatever.com what+ever@whatever.ca]
+	    valid_addresses do |valid_address|
+	    	@user.email = valid_address
+	    	assert @user.valid?, "#{valid_address.inspect} is valid"
+	    end
+	end
+
+	test "email should reject an invalid email format" do
+		invalid_addresses = %w[whatever@whatever,com whatever.org what.ever@whatever.
+                               whatever@what_ever.com whatever@what+ever.com]
+        invalid_addresses do |invalid_address|
+        	@user.email = invalid_address
+        	assert_not @user.valid? "#{invalid_address.inspect} is not valid"
+        end
+    end
+
+    test "email database should not allow duplicates" do
+    	possible_duplicate = @user.dup
+    	#to make sure when the format to be uppercase it still works
+    	possible_duplicate.email = @user.email.uppercase
+    	@user.save
+    	#after use itself gets saved, then its duplicate will not be allowed
+    	assert_not possible_duplicate.valid?
+    end
+end
