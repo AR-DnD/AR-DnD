e03f650c21014975a9b30e6a0082770fdae5ab8e
Akiba Sato <akibasato1992@gmail.com>
2017-10-27 15:53:39 -0400

    Edited tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :adventures
   has_many :maps, through: :adventures
 
+  attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
 
@@ -41,6 +42,11 @@ class User < ActiveRecord::Base
 
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
 end
