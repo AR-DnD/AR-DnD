865fb5cf97935e02fdb39e16c91fc6740d783632
CAM <cbraunstein@brandeis.edu>
2017-10-31 23:35:24 -0400

    Added size bar attribute to map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -78,7 +78,7 @@ class MapsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def map_params
-      params.require(:map).permit(:name, :data, :story, :adventure)
+      params.require(:map).permit(:name, :data, :story, :adventure, :size)
     end
 
 
