ca8ba6b4553b5037c2c2319b7440b00c8a361cf7
Akiba Sato <akibasato1992@gmail.com>
2017-11-10 15:21:32 -0500

    Edited welcome page routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  def users_exist
+    User.all.size > 0
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
