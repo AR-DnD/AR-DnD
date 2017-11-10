2343b034c5ea5e43784dac7249eaab2782e60dbe
Akiba Sato <akibasato1992@gmail.com>
2017-11-10 15:30:20 -0500

    Undid mistake.

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 
 module ARDndApp
   class Application < Rails::Application
