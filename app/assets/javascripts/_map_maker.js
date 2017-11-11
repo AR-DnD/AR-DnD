640ccf028e0b4f9d6b185226ab79ff91d5a294e3
CAM <cbraunstein@brandeis.edu>
2017-11-11 14:13:12 -0500

    Added tutorial divs

--- app/assets/javascripts/_map_maker.js
+++ app/assets/javascripts/_map_maker.js
@@ -1,7 +1,11 @@
 $(document).on('turbolinks:load', function(){
   $("input[type='submit'][name='commit'][value='Create Map']").prop("disabled",true);
 
-  var grid = Array(3).fill().map(x >> Array(3).fill(null))
+  //var grid = Array(3).fill().map(x >> Array(3).fill(null))
+  var grid = new Array(3);
+  for (i=0; i<grid.length; i++){
+    grid[i]=new Array(3).fill(null)
+  }
   console.log(grid)
   var currElement = "Tree"
   //var grid = [[null, null, null], [null, null, null], [null, null, null]]
@@ -86,6 +90,6 @@ $(document).on('turbolinks:load', function(){
       $("input[type='submit'][name='commit'][value='Create Map']").prop("disabled",true);
     } else {
       $("input[type='submit'][name='commit'][value='Create Map']").prop("disabled",false);
-    }of
+    }
   })
 });
