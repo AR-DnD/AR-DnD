6341313ba456c249339832d4fa64853b6291950e
Akiba Sato <akibasato1992@gmail.com>
2017-11-17 14:48:35 -0500

    Edited pages for when there are no users in database.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
 
   def home
-    if users_exist
-      if logged_in?
-        redirect_to user_path(current_user)
-      end
-    end
+    # if users_exist
+    #   if logged_in?
+    #     redirect_to user_path(current_user)
+    #   end
+    # end
   end
 
   def about
