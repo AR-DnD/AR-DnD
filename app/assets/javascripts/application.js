5e050514354e03f7fb087f6d473d3b267774a7a5
Akiba Sato <akibasato1992@gmail.com>
2017-11-19 13:21:20 -0500

    Edited login/signup modal button controls.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,6 @@
 //= require bootstrap-sprockets
 //= require_tree
 //= require jquery-ui
+
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
