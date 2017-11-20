a1e9a851eeec597306315e6ca3c9dee32f1d91e0
Akiba Sato <akibasato1992@gmail.com>
2017-11-20 13:05:00 -0500

    Edited a few tests.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,15 +7,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with remembering" do
-    log_in_as(@user, remember_me: '1')
+    log_in(@user, remember_me: '1')
     assert_not_empty cookies['remember_token']
   end
 
   test "login without remembering" do
     # Log in to set the cookie.
-    log_in_as(@user, remember_me: '1')
+    log_in(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
-    log_in_as(@user, remember_me: '0')
+    log_in(@user, remember_me: '0')
     assert_empty cookies['remember_token']
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,12 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "name is too long" do
-		@user.name = "a"*16
+		@user.username = "a"*16
 		assert_not @user.valid?
 	end
 
 	test "name is too short" do
-		@user.name = "a"*2
+		@user.username = "a"*2
 		assert_not @user.valid?
 	end
 
@@ -32,26 +32,26 @@ class UserTest < ActiveSupport::TestCase
 	test "email should reject an invalid email format" do
 		invalid_addresses = %w[whatever@whatever,com whatever.org what.ever@whatever.
                                whatever@what_ever.com whatever@what+ever.com]
-        invalid_addresses do |invalid_address|
-        	@user.email = invalid_address
-        	assert_not @user.valid? "#{invalid_address.inspect} is not valid"
-        end
-    end
+      invalid_addresses do |invalid_address|
+      	@user.email = invalid_address
+      	assert_not @user.valid? "#{invalid_address.inspect} is not valid"
+      end
+  end
 
-    test "email database should not allow duplicates" do
-    	possible_duplicate = @user.dup
-    	#to make sure when the format to be uppercase it still works
-    	possible_duplicate.email = @user.email.uppercase
-    	@user.save
-    	#after use itself gets saved, then its duplicate will not be allowed
-    	assert_not possible_duplicate.valid?
-    end
+  test "email database should not allow duplicates" do
+  	possible_duplicate = @user.dup
+  	#to make sure when the format to be uppercase it still works
+  	possible_duplicate.email = @user.email.uppercase
+  	@user.save
+  	#after use itself gets saved, then its duplicate will not be allowed
+  	assert_not possible_duplicate.valid?
+  end
 
   # test "the truth" do
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(username: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
     test "authenticated? should return false for a user with nil digest" do
