77f090dc26541ed1cd804d301b849486f3b8db22
CAM <cbraunstein@brandeis.edu>
2017-11-29 12:10:44 -0500

    Changed login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:success] = "You have successfully logged in."
-            redirect_to root_path
+            #redirect_to root_path
+            redirect_to user_path(user.id)
         else
             flash.now[:danger] = "There was something wrong with your login information."
             render 'new'
