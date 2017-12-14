6ee773ad330309cd60fc9afed2b7a910f6c90cf2
CAM <cbraunstein@brandeis.edu>
2017-12-14 17:40:16 -0500

    First success testing users

--- test/controllers/characters_controller_test.rb
+++ test/controllers/characters_controller_test.rb
@@ -1,48 +1,49 @@
-# require 'test_helper'
-#
-# class CharactersControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     @character = characters(:one)
-#   end
-#
-#   test "should get index" do
-#     get characters_url
-#     assert_response :success
-#   end
-#
-#   test "should get new" do
-#     get new_character_url
-#     assert_response :success
-#   end
-#
-#   test "should create character" do
-#     assert_difference('Character.count') do
-#       post characters_url, params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
-#     end
-#
-#     assert_redirected_to character_url(Character.last)
-#   end
-#
-#   test "should show character" do
-#     get character_url(@character)
-#     assert_response :success
-#   end
-#
-#   test "should get edit" do
-#     get edit_character_url(@character)
-#     assert_response :success
-#   end
-#
-#   test "should update character" do
-#     patch character_url(@character), params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
-#     assert_redirected_to character_url(@character)
-#   end
-#
-#   test "should destroy character" do
-#     assert_difference('Character.count', -1) do
-#       delete character_url(@character)
-#     end
-#
-#     assert_redirected_to characters_url
-#   end
-# end
+require 'test_helper'
+
+class CharactersControllerTest < ActionDispatch::IntegrationTest
+  # setup do
+  #   current_user = User.first
+  #   @character = characters(:one)
+  # end
+  #
+  # test "should get index" do
+  #   get user_characters_url(@character)
+  #   assert_response :success
+  # end
+
+  # test "should get new" do
+  #   get new_user_character_url
+  #   assert_response :success
+  # end
+  #
+  # test "should create character" do
+  #   assert_difference('Character.count') do
+  #     post user_characters_url, params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
+  #   end
+  #
+  #   assert_redirected_to character_url(Character.last)
+  # end
+  #
+  # test "should show character" do
+  #   get user_character_url(@character)
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get edit_user_character_url(@character)
+  #   assert_response :success
+  # end
+  #
+  # test "should update character" do
+  #   patch user_character_url(@character), params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
+  #   assert_redirected_to user_character_url(@character)
+  # end
+  #
+  # test "should destroy character" do
+  #   assert_difference('Character.count', -1) do
+  #     delete user_character_url(@character)
+  #   end
+  #
+  #   assert_redirected_to user_characters_url
+  # end
+end
