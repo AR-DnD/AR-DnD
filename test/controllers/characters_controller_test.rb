339238517f628a8cdbeeb6709bd810538b976667
CAM <cbraunstein@brandeis.edu>
2017-12-15 16:46:08 -0500

    Making progress on character tests

--- test/controllers/characters_controller_test.rb
+++ test/controllers/characters_controller_test.rb
@@ -1,33 +1,42 @@
 require 'test_helper'
 
 class CharactersControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   current_user = User.first
-  #   @character = characters(:one)
-  # end
-  #
-  # test "should get index" do
-  #   get user_characters_url(@character)
-  #   assert_response :success
-  # end
+  setup do
+    @user = users(:one)
+    post login_path, params: {
+      session: {
+        user_id: @user.id,
+        email: @user.email,
+        password: 123456
+      }
+    }
+    @character = characters(:one)
+  end
 
-  # test "should get new" do
-  #   get new_user_character_url
-  #   assert_response :success
-  # end
-  #
-  # test "should create character" do
-  #   assert_difference('Character.count') do
-  #     post user_characters_url, params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
-  #   end
-  #
-  #   assert_redirected_to character_url(Character.last)
-  # end
-  #
-  # test "should show character" do
-  #   get user_character_url(@character)
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get user_characters_url(@character)
+    assert_response :success
+  end
+
+  test "should get new" do
+
+    get new_user_character_url(user_id: @user.id)
+    assert_response :success
+  end
+
+  test "should create character" do
+    assert_difference('Character.count') do
+      post user_characters_url(user_id: @user.id, id: @character.id), params: { character: { charisma: @character.charisma, constitution: @character.constitution, dexterity: @character.dexterity, intelligence: @character.intelligence, level: @character.level, name: @character.name, profession: @character.profession, race: @character.race, strength: @character.strength, wisdom: @character.wisdom } }
+    end
+
+    assert_redirected_to user_url(@user)
+  end
+
+  test "should show character" do
+    byebug
+    get user_character_url(user_id: @user.id, id: @character.id)
+    assert_response :success
+  end
   #
   # test "should get edit" do
   #   get edit_user_character_url(@character)
