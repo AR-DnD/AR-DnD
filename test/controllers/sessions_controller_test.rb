b6cbdb7b98d66d3676f970f327a24a769125246b
CAM <cbraunstein@brandeis.edu>
2017-12-16 15:31:29 -0500

    Added full test suite

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,16 @@
-# require 'test_helper'
-#
-# class SessionsControllerTest < ActionDispatch::IntegrationTest
-#
-#   test "should get new" do
-#     get login_path
-#     assert_response :success
-#   end
-# end
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get new" do
+    @user = users(:one)
+    post login_path, params: {
+      session: {
+        user_id: @user.id,
+        email: @user.email,
+        password: 123456
+      }
+    }
+    assert_redirected_to user_url(@user.id)
+  end
+end
